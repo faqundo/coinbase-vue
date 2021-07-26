50c9254031c496b122169f6149f0bb31d8bc98b9
faqundo <facundo.ferro@werden.com.ar>
2021-07-25 23:48:38 -0300

    Welcome page commit

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,22 +2,25 @@
   <div class="home">
     <img alt="background" src="../assets/BG.png" class="background" />
     <img alt="img cripto" src="../assets/IMG.png" class="img-welcome" />
-    <div class="title">
-      <h1>hola</h1>
-      <h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nam culpa
-        voluptatem vero tenetur quibusdam sint, laboriosam aspernatur vitae,
-        odio repellendus accusantium fuga ea repellat magni corrupti consectetur
-        nihil labore!
-      </h3>
-      <vs-button
-        transparent
-        :active="btnActive == 1"
-        @click="btnActive = 1"
-        class="container-btn"
-      >
-        <img :src="getStarted" alt="" class="btn-get-started" />
-      </vs-button>
+    <div class="row">
+      <div class="col-6 container-left">
+        <div class="title">
+          We provide easy solution to exchange your
+          <b>Bitcoin/ GiftCard for money</b>
+        </div>
+        <div class="subtitle">
+          CoinBase is a platform for trading your bitcoin and giftcard at the
+          best rate, why not give us a trial.
+        </div>
+        <vs-button
+          transparent
+          :active="btnActive == 1"
+          @click="btnActive = 1"
+          class="container-btn"
+        >
+          <img :src="getStarted" alt="" class="btn-get-started" />
+        </vs-button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,6 +29,7 @@
 // @ is an alias to /src
 /* import HelloWorld from '@/components/HelloWorld.vue' */
 import getStarted from "@/assets/getStarted.png";
+
 
 export default {
   name: "Home",
@@ -41,6 +45,9 @@ export default {
 </script>
 
 <style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,200&display=swap');
+
 .background {
   position: absolute;
   width: 1613px;
@@ -53,12 +60,49 @@ export default {
   height: 519px;
   left: 828px;
 }
-.title {
+.container-left {
   position: absolute;
+  text-align: left;
+  padding: 142px;
+  padding-right: 0;
+}
+.title {
+  height: 180px;
+  display: contents;
+
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 60px;
+
+  color: #ffffff;
+  padding-bottom: 30px;
+}
+.subtitle {
+  width: 460px;
+  height: 54px;
+
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 27px;
+
+  color: #ffffff;
+  padding-top:25px;
+  padding-bottom:70px
+}
+.container-btn {
+  padding: 0;
+  margin: 0;
 }
 .btn-get-started {
   width: 157px;
   height: 54px;
   border-radius: 100px;
+  padding: 0;
+  margin: 0;
+  
 }
 </style>
